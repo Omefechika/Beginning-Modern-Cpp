8d8f6801228f6a0a06f68b94041b7d6bfa0828a0
Omefechika <omefechika@gmail.com>
2025-02-15 01:49:29 +0100

    Using the sort and binary_search function with array

---
+++ sortNsearch/sortAndBinarySearch.cpp
@@ -0,0 +1,18 @@
+// Sorting and searching arrays using the sort and
+// binary_search functions
+#include <cstddef>
+#include <iostream>
+#include <iomanip>
+#include <array>
+#include <string>
+#include <algorithm> // contains sort ans binary_Search
+using namespace std;
+
+int main()
+{
+  const size_t arraySize{7};
+  array<string, arraySize> colors{};
+      
+}
+
+  

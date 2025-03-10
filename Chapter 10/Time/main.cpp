e3181905615126fed5f89c1a2af87eedbf14fcff
Omefechika <omefechika@gmail.com>
2025-03-10 14:51:59 +0100

    restore deleted files

--- Chapter 10/Time/main.cpp
+++ Chapter 10/Time/main.cpp
@@ -7,17 +7,19 @@
 using namespace std;
 
 // display a Time in 24-hour and 12-hour formats
-void displayTime(const string &message, const Time &time){
+void displayTime(const string &message, const Time &time)
+{
     cout << message << "\nUniversal time: " << time.toUniversalString()
-    << "\nStandard time: " << time.toStandardString() << "\n\n";
+         << "\nStandard time: " << time.toStandardString() << "\n\n";
 }
 
-int main(){
+int main()
+{
     Time t; // Instantiate object t of class Time
 
     displayTime("Initial Time:", t); // display t's initial value (00:00:00)
-    
-    t.setTime(13, 27, 6); // change time
+
+    t.setTime(13, 27, 6);             // change time
     displayTime("After setTime:", t); // display new time after time set
 
     // attempt time set with invalid values
@@ -25,12 +27,11 @@ int main(){
     {
         t.setTime(99, 99, 99); // out of range values
     }
-    catch(invalid_argument& e)  
+    catch (invalid_argument &e)
     {
         cout << "Exception: " << e.what() << "\n\n";
     }
 
     // display t's value after invalid value range
     displayTime("After attempting to set an invalid time:", t);
-    
 }
